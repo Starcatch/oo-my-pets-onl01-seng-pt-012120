15430762370da21ab0fe3dcad5ecde1f5e503286
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 23:02:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,12 @@
 class Cat
   attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
+  attr_accessor :mood, :owner
+ @@cats = []
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
+    @@cats << self
+    owner.cats << self 
   end
 end
