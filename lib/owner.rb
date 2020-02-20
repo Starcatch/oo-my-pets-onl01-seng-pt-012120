55ae97c23f7ab4e1ac062e86ddd808ed999012de
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 22:48:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,11 +2,13 @@ class Owner
   attr_accessor :cat, :dog
    # can have a name
      # cannot change owner's name
+     # initializes with species set to human
+      #can't change its species
    attr_reader :name, :species 
    
    @@owners = []
    
-   def initialize(name)
+   def initialize(species)
      @name = name
      @species = "human"
      @@owners << self 
