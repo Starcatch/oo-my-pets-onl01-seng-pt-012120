a11822d4e8328bfca2fe08bb2d5c034b3b576d90
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 22:24:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
+class Fish
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+end
+
+class Cat
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+end
+
+class Dog
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+end
+
 class Owner
   attr_reader :species
   attr_accessor :name, :pets, :fish, :dog, :cat
@@ -10,4 +41,68 @@ class Owner
     @name = name
     @@all << self
   end
-end 
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def pets
+    @@pets
+  end
+
+  def buy_fish(fish)
+    @@pets[:fishes] << Fish.new(fish)
+  end
+
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+      dog.mood = 'happy'
+    end
+  end
+
+  def play_with_cats
+    @@pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
+  end
+
+  def feed_fish
+    @@pets[:fishes].each do |fish|
+      fish.mood = 'happy'
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
+  def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
+  end
+
+end
