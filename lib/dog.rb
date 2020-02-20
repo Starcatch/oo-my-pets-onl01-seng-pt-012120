71cb733705e76f789f4cf86858de00ee807e769d
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 23:06:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog
   attr_reader :name
-  attr_accessor :mood
+  attr_accessor :mood, :owner 
 
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
   end
