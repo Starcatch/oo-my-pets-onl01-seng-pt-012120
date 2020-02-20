15ecada814e5904eee6afc304b83efda9564fa19
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 22:35:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,4 @@
 class Owner
   attr_accessor :pets, :name
-  attr_reader :species
-
-  @@all = []
-
-  def initialize(species)
-    @species = species
-    @pets = {fishes: [], cats: [], dogs: []}
-    @@all << self
-  end
+ 
 end 
